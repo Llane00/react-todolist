9ad12926e21a625df9100b56bb50a99769d6645f
Llane00 <bluetinestyle@126.com>
2021-10-28 17:29:29 +0800

    feat: complete AddInput addItem action

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import { AddInput } from './components/AddInput';
 import { Header as MyHeader } from './components/Header';
 
 function App() {
   const [inputVisible, setInputVisible] = useState(false)
+  const [todolist, setTodolist] = useState([])
+
+  const addItem = useCallback((value) => {
+    const item = {
+      id: new Date().getTime(),
+      content: value,
+      completed: false
+    }
+
+    setTodolist(list => [...list, item])
+    setInputVisible(false)
+  }, [])
 
   return (
     <div className="App">
       <MyHeader switchInputVisible={() => setInputVisible(!inputVisible)} />
-      <AddInput inputVisible={inputVisible}/>
+      <AddInput inputVisible={inputVisible} addItem={ addItem }/>
     </div>
   );
 }
--- src/components/AddInput/index.js
+++ src/components/AddInput/index.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./index.scss"
 
 export const AddInput = (props) => {
-  const { inputVisible } = props;
+  const { inputVisible, addItem } = props;
+
+  const inputRef = useRef()
+
+  const submitNewTodoItem = () => {
+    const inputValue = inputRef.current.value.trim();
+
+    if (inputValue.length === 0) {
+      return;
+    }
+
+    addItem(inputValue)
+
+    inputRef.current.value = "";
+  }
 
   return (
     <>
@@ -10,10 +24,13 @@ export const AddInput = (props) => {
         inputVisible && (
           <div className="input-wrapper">
             <input
+              ref={inputRef}
+              type="text"
               placeholder="请输入待办事项"
             />
             <button
               className="btn btn-primary"
+              onClick={ submitNewTodoItem }
             >
               增加
             </button>
