0a1cd4a8e53064a5289943ccc4f46e1c618f3c17
Llane00 <bluetinestyle@126.com>
2021-10-28 18:30:10 +0800

    feat: add TodoItem components and set data to localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
 import { AddInput } from './components/AddInput';
 import { Header as MyHeader } from './components/Header';
+import { TodoItem } from './components/TodoItem';
 
 function App() {
   const [inputVisible, setInputVisible] = useState(false)
-  const [todolist, setTodolist] = useState([])
+  const [todoList, setTodoList] = useState([])
 
+  useEffect(() => {
+    const todoData = JSON.parse(localStorage.getItem("todoData") || "[]")
+    setTodoList(todoData)
+    return () => {}
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todoData", JSON.stringify(todoList))
+  }, [todoList])
+  
   const addItem = useCallback((value) => {
     const item = {
       id: new Date().getTime(),
@@ -14,14 +25,25 @@ function App() {
       completed: false
     }
 
-    setTodolist(list => [...list, item])
+    setTodoList(list => [...list, item])
     setInputVisible(false)
   }, [])
 
   return (
     <div className="App">
       <MyHeader switchInputVisible={() => setInputVisible(!inputVisible)} />
-      <AddInput inputVisible={inputVisible} addItem={ addItem }/>
+      <AddInput inputVisible={inputVisible} addItem={addItem} />
+      <ul className="todo-list">
+        {
+          todoList &&
+          todoList.map((item) => 
+            <TodoItem
+              data={item}
+              key={item.id}
+            />
+          )
+        }
+      </ul>
     </div>
   );
 }
